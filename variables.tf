10d94389e7f0073e8fd710aa950fedd9f8a150f3
Kevin Pedersen <byte_count@hotmail.com>
2016-11-17 18:29:47 -0500

    troubleshooting list variables, declaring

--- variables.tf
+++ variables.tf
@@ -17,6 +17,12 @@ variable delonterm { default = true }
 variable volsize { default = 40 }
 
 
-variable "public_subnets" { default = [] }         #list of public subnet ids for the target VPC
-variable "vpc_security_group_ids" { default = [] } #list of security group ids to make the bastion member of
-variable "vpc_id" {}                               #vpc id for the target VPC
+variable "public_subnets" {
+  type = "list"
+}
+
+variable "vpc_security_group_ids" {
+  type = "list"
+}
+
+variable "vpc_id" {}
